c25b513d10a72c6f0ee3773d9fb55eabf5706061
Nícolas Wagner Barbosa Pereira - NPE <nicolas.pereira@inote549.lan>
2020-12-16 23:28:10 -0300

    handling submit and change on Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Form(props) {
-  return(
+  const [name, setName] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.addTask("name");
+    setName("");
+  }
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  return (
     <div className="form-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2 className="label-wrapper">
           <label htmlFor="new-todo-input" className="label_lg">
             O que precisa ser feito?
           </label>
         </h2>
-        <input type="text" id="new-todo-input" className="input input_lg" name="text" autoComplete="off"/>
-        <button type="submit" className="btn btn_primary btn_lg">Adicionar</button>
+        <input
+          type="text"
+          id="new-todo-input"
+          className="input input_lg"
+          name="text"
+          autoComplete="off"
+          value={name}
+          onChange={handleChange}
+        />
+        <button type="submit" className="btn btn_primary btn_lg">
+          Adicionar
+        </button>
       </form>
     </div>
   );
 }
-
 export default Form;
