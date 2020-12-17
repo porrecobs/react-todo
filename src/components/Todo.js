ed6e5550c371a2af53473df718325abddfcd8ae9
Nícolas Wagner Barbosa Pereira - NPE <nicolas.pereira@inote549.lan>
2020-12-17 00:35:47 -0300

    adding delete and toggle completed to Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Todo(props) {
-  return(<li className="todo stack-small">
-  <div className="c-cb">
-  <input id={props.id} type="checkbox" defaultChecked={props.completed}/>
-  <label className="todo-label" htmlFor={props.id}>
-    {props.name}
-  </label>
-  </div>
-  <div className="btn-group">
-    <button type="button" className="btn">
-      editar <span className="visually-hidden">Tarefa 0</span>
-    </button>
-    <button type="button" className="btn btn_danger">
-      deletar <span className="visually-hidden">Tarefa 0</span>
-    </button>
-  </div>
-</li>);
+  const [isEditing, setEditing] = useState(false);
+  return (
+    <li className="todo stack-small">
+      <div className="c-cb">
+        <input
+          id={props.id}
+          type="checkbox"
+          defaultChecked={props.completed}
+          onChange={() => props.toggleTaskCompleted(props.id)}
+        />
+        <label className="todo-label" htmlFor={props.id}>
+          {props.name}
+        </label>
+      </div>
+      <div className="btn-group">
+        <button type="button" className="btn">
+          editar <span className="visually-hidden">{props.name}</span>
+        </button>
+
+        <button
+          type="button"
+          className="btn btn_danger"
+          onClick={() => props.deleteTask(props.id)}
+        >
+          deletar <span className="visually-hidden">{props.name}</span>
+        </button>
+      </div>
+    </li>
+  );
 }
