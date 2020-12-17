89f21d84898955a36653c217e7742f075231259f
Nícolas Wagner Barbosa Pereira - NPE <nicolas.pereira@inote549.lan>
2020-12-16 22:01:06 -0300

    creating FilterButton

---
+++ src/components/FilterButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function FilterButton(props) {
+  return(
+    <button type="button" className="btn toggle-btn" aria-pressed="true">
+      <span className="visually-hidden">exibir </span>
+      <span>tudo</span>
+      <span className="visually-hidden"> tarefas</span>
+    </button>
+  )
+}
+
+export default FilterButton;
