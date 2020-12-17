617c144567a6b31f8271fffe8cf44af3f03c5901
Nícolas Wagner Barbosa Pereira - NPE <nicolas.pereira@inote549.lan>
2020-12-17 01:33:43 -0300

    adding setFilter to FilterButton

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -2,7 +2,12 @@ import React from "react";
 
 function FilterButton(props) {
   return (
-    <button type="button" className="btn toggle-btn" aria-pressed="true">
+    <button
+      type="button"
+      className="btn toggle-btn"
+      aria-pressed={props.isPressed}
+      onClick={() => props.setFilter(props.name)}
+    >
       <span className="visually-hidden">exibir </span>
       <span>{props.name}</span>
       <span className="visually-hidden"> tarefas</span>
