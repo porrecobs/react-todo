0ad60de5107ed3c54c59aa18042ebde3b0dcd5d8
Nícolas Wagner Barbosa Pereira - NPE <nicolas.pereira@inote549.lan>
2020-12-16 23:29:57 -0300

    linting and removing reportWebVitals

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
+/* eslint-disable jsx-a11y/no-redundant-roles */
 import Todo from "./components/Todo";
 import Form from "./components/Form";
-import './App.css';
+import "./App.css";
 import FilterButton from "./components/FilterButton";
 
 function App(props) {
-  const taskList = props.tasks.map(task => (
-    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id}/>));
+  const taskList = props.tasks.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
+  ));
+
+  function addTask(name) {}
+
   return (
     <div className="todo-app">
       <h1>2-Do</h1>
-      <Form addTask={addTask}/>
+      <Form addTask={addTask} />
       <div className="filters btn-group">
         <FilterButton />
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">
-        3 tarefas restantes
-      </h2>
-      <ul role="list" className="todo-list stack-large stack-exception" aria-labelledby="list-heading">
+      <h2 id="list-heading">3 tarefas restantes</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
         {taskList}
       </ul>
     </div>
-  );
-}
-
-function addTask(name) {
-  super.taskList.push(
-    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id}/>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
 
 const DEFAULT_DATA = [
   { id: "todo-0", name: "comer", completed: true },
   { id: "todo-1", name: "rezar", completed: false },
-  { id: "todo-2", name: "amar", completed: false }
-]
+  { id: "todo-2", name: "amar", completed: false },
+];
 
-ReactDOM.render(
-  <App tasks={DEFAULT_DATA} />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App tasks={DEFAULT_DATA} />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
