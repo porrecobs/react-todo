ae2c4dce96ef78d16be167a2fad2ae70bcceb189
Nícolas Wagner Barbosa Pereira - NPE <nicolas.pereira@inote549.lan>
2020-12-16 21:38:43 -0300

    creating taskList

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import logo from './logo.svg';
 import Todo from "./components/Todo";
 import './App.css';
 
-function App() {
+function App(props) {
+  const taskList = props.tasks.map(task => (
+    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id}/>));
   return (
     <div className="todo-app">
       <h1>2Do</h1>
@@ -36,24 +38,7 @@ function App() {
         3 tarefas restantes
       </h2>
       <ul role="list" className="todo-list stack-large stack-exception" aria-labelledby="list-heading">
-        <Todo name="comer"/>
-        <Todo name="sleep" />
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-2" type="checkbox" />
-            <label className="todo-label" htmlFor="todo-2">
-              Repeat
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Repeat</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Repeat</span>
-            </button>
-          </div>
-        </li>
+        {taskList}
       </ul>
     </div>
   );
